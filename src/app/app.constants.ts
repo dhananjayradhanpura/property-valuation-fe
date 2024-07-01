3ec9addb2d872bbb5ed4c906d3168f151e97d409
dhananjayradhanpura <35652048+dhananjayradhanpura@users.noreply.github.com>
2024-07-01 18:01:02 +0530

    first commit

---
+++ src/app/app.constants.ts
@@ -0,0 +1,15 @@
+export const AppConstants = {
+    HOST:'http://localhost:8080',
+    AUTH_URL: '/api/v1/auth',
+    SIGN_IN_URL: '/signin'
+}
+export const ApiConstants = {
+    DROPDOWN_URL: '/api/v1/dropdown',
+    CREATE_FORM_URL: '/api/v1/propertyval/evaluation-form',
+    NEXT_REF_NO: '/api/v1/propertyval/nextappreference',
+    FETCH_APPLICATIONS: '/api/v1/propertyval/fetchapplications',
+    FETCH_COMMENTS: '/api/v1/comment/comments',
+    FETCH_DOCUMENTS:'/api/v1/document/documents'
+}
+
+
